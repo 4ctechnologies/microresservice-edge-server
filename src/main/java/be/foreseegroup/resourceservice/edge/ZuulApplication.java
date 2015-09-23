2634039e721ff4300f306e7540b493527b24bc9e
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2015-09-23 13:24:33 +0200

    Initial commit

---
+++ src/main/java/be/foreseegroup/resourceservice/edge/ZuulApplication.java
@@ -0,0 +1,19 @@
+package be.foreseegroup.resourceservice.edge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Kaj on 21/09/15.
+ */
+
+@SpringBootApplication
+@Controller
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
+}
